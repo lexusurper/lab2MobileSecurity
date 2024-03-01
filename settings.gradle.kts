1d05cc37e5e3aea73ef4262c7645c1ffffacb8c2
lexusurper <kabdollaev01@mail.ru>
2024-03-01 23:07:46 +0500

    -

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "sis2125YernurRaimbekWorksheet3"
+include(":app")
+ 
